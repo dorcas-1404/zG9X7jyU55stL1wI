8f10ba743cad9f5d02c3e98491d817b74b427ede
dorcas-1404 <dorcasakalo@gmail.com>
2023-06-16 00:20:40 -0400

    second commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project explored and identify ways to improve the success rate for calls made to customers to subscribe for a term deposit product.',
+    author='Dorcas Taiwo',
+    license='MIT',
+)
